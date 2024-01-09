1b6b24be0c67dbd2bdc74139f07061d779e632f6
EmeteZ <mtmacedo17@gmail.com>
2024-01-09 20:48:22 -0300

    Manipulação de Listas

--- React Sessão 3/avancando/src/components/ListRender.js
+++ React Sessão 3/avancando/src/components/ListRender.js
@@ -8,13 +8,38 @@ const ListRender = () => {
     "Robson",
     "Gisele",
   ]);
+
+  const [users, setUsers] = useState([
+    { id: 1, name: "Matheus M", age: 17 },
+    { id: 2, name: "Kratos", age: 1032 },
+    { id: 3, name: "Jonny Cage", age: 26 },
+  ]);
+
+  const deleteRandom = () => {
+    const randomNumber = Math.floor(Math.random() * 4);
+
+    setUsers((prevUsers) => {
+      return prevUsers.filter((user) => randomNumber !== user.id);
+    });
+  };
+
   return (
     <div>
       <ul>
-        {list.map((item) => (
-          <li>{item}</li>
+        {list.map((item, i) => (
+          <li key={i}>{item}</li>
         ))}
       </ul>
+      <ul>
+        {" "}
+        {users.map((user) => (
+          <li key={user.id}>
+            {" "}
+            {user.name} - {user.age}
+          </li>
+        ))}
+      </ul>
+      <button onClick={deleteRandom}>Deletar Usuario Random</button>
     </div>
   );
 };

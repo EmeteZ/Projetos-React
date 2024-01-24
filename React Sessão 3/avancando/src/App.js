c6849bd7681a8572721bc2590ee003a3589c7a66
EmeteZ <mtmacedo17@gmail.com>
2024-01-23 23:22:30 -0300

    Final sessão 3

--- React Sessão 3/avancando/src/App.js
+++ React Sessão 3/avancando/src/App.js
@@ -1,12 +1,28 @@
+import { useState } from "react";
 import "./App.css";
+import Fragment from "./components/Fragment";
 import CarDetails from "./components/CarDetails";
 import CondicionalRender from "./components/CondicionalRender";
 import ListRender from "./components/ListRender";
 import ManageData from "./components/ManageData";
 import ShowUserName from "./components/ShowUserName";
 import LogoVL from "./images/corinthiansold.png";
+import Container from "./components/Container";
+import ExecuteFunction from "./components/ExecuteFunction";
+import Message from "./components/Message";
+import ChangeMessageState from "./components/ChangeMessageState";
 
 function App() {
+  function showMessage() {
+    console.log("Evento do componente pai");
+  }
+
+  const [message, setMessage] = useState("");
+
+  const handleMessage = (msg) => {
+    setMessage(msg);
+  };
+
   return (
     <div className="App">
       <h1> Avançando em React </h1>
@@ -19,6 +35,13 @@ function App() {
       <CondicionalRender />
       <ShowUserName name="Matheus" />
       <CarDetails brand="VW" km={100000} color="Azul" />
+      <Fragment />
+      <Container>
+        <p>Este é o conteudo</p>
+      </Container>
+      <ExecuteFunction myFunction={showMessage} />
+      <Message msg={message} />
+      <ChangeMessageState handleMessage={handleMessage} />
     </div>
   );
 }
---
+++ React Sessão 3/avancando/src/components/Fragment.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Fragment = () => {
+  return (
+    <>
+      <h1>Titulo</h1>
+      <h1>Titulo 2</h1>
+    </>
+  );
+};
+
+export default Fragment;

a0d38a2aaabdc76e084ae9ec899340452f5f56de
EmeteZ <mtmacedo17@gmail.com>
2024-07-16 21:15:22 -0300

    Andamento da Sessão CSS

--- React Sessão 4/css/src/App.js
+++ React Sessão 4/css/src/App.js
@@ -1,9 +1,23 @@
 import "./App.css";
+import Caixa from "./components/Caixa.js";
 
 function App() {
+  const n = 10;
+  const redTitle = true;
   return (
     <div className="App">
       <h1>React com CSS</h1>
+      <p>Texto</p>
+      <Caixa />
+      <p style={{ color: "green" }}>Texo Inline</p>
+      <h2 style={n > 10 ? { color: "purple" } : { color: "pink" }}>
+        CSS com requisito
+      </h2>
+      <h2 style={n < 15 ? { color: "purple" } : { color: "pink" }}>
+        CSS com requisito
+      </h2>
+
+      <h2 className={redTitle ? "red-title" : "blue-title"}>TITULO KK</h2>
     </div>
   );
 }
---
+++ React Sessão 4/css/src/components/Caixa.js
@@ -0,0 +1,7 @@
+import "./Caixa.css";
+
+const Caixa = () => {
+  return <p className="caixa">CAIXA</p>;
+};
+
+export default Caixa;

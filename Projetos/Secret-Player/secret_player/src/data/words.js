9bd2aeb14c8902a7535ccabe5666909fa1944719
EmeteZ <mtmacedo17@gmail.com>
2024-07-25 23:58:19 -0300

    Criação do Projeto Secret Player

---
+++ Projetos/Secret-Player/secret_player/src/data/words.js
@@ -0,0 +1,8 @@
+export const wordsList = {
+  Flamengo: ["Gabigol", "Arrascaeta", "Pedro", "Bruno Henrique", "Delacruz"],
+  Palmeiras: ["Dudu", "Weverton", "Rony", "Veiga", "Estevão"],
+  Corinthians: ["Hugo", "Fagner", "Garro", "Wesley", "Romero"],
+  Real_Madrid: ["Vinicius Junior", "Rodrygo", "Modric", "Bellingham", "Mbappe"],
+  Barcelona: ["Yamal", "De Jong", "Gavi", "Araujo", "Raphinha"],
+  Lendas_Brasileiras: ["Pelé", "Zico", "Ronaldo", "Romário", "Neymar"],
+};
